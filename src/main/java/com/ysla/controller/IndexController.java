423c700148f53c4e950827f2a154836b2d83c216
konghanghang <ysla@pm.me>
2018-06-27 14:24:03 +0800

    shiro和spring整合

--- src/main/java/com/ysla/controller/IndexController.java
+++ src/main/java/com/ysla/controller/IndexController.java
@@ -1,7 +1,13 @@
 package com.ysla.controller;
 
+import com.ysla.vo.User;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,6 +17,20 @@ public class IndexController {
     @ResponseBody
     public String index(){
         return "welcome to shiro";
+    }
+
+    @RequestMapping(value = "/login1", method = RequestMethod.POST,produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String login(User user){
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(user.getUsername(),
+                user.getPassword());
+        try {
+            subject.login(usernamePasswordToken);
+        } catch (AuthenticationException e) {
+            return e.getMessage();
+        }
+        return "login success,welcome to shiro!";
     }
 
 }
